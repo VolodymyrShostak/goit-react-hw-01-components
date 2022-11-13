e21e560bd8947683d30ba73d66888324a60c2873
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-11-13 03:22:55 +0200

    creation component

--- src/index.js
+++ src/index.js
@@ -1,10 +1,20 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { App } from 'components/App';
+import { App } from './App';
 import './index.css';
 
-ReactDOM.createRoot(document.getElementById('root')).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+// ReactDOM.createRoot(document.getElementById('root')).render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+// import { createRoot } from 'react-dom/client';
+
+// import { App } from './App';
+
+// ReactDOM.render(
+//
+//   document.querySelector('#root')
+// );
+ReactDOM.render(<App />, document.querySelector('#root'));
