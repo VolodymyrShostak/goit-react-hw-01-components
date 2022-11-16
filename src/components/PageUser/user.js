887cbc7d4ebc099667b461c6dca543e18505d9d0
VolodymyrShostak <110408009+VolodymyrShostak@users.noreply.github.com>
2022-11-16 19:40:03 +0200

    add style to task2

--- src/components/PageUser/user.js
+++ src/components/PageUser/user.js
@@ -9,31 +9,34 @@ export default function Profile({
   stats: { followers, views, likes },
 }) {
   return (
-    <div className={css.profile}>
-      <div className={css.description}>
-        <img src={avatar} alt="User avatar" className={css.avatar} />
-        <p className={css.name}>{username}</p>
-        <p className={css.tag}>{tag}</p>
-        <p className={css.location}>{location}</p>
-      </div>
+    <div className={css.container}>
+      <div className={css.profile}>
+        <div className={css.description}>
+          <img src={avatar} alt="User avatar" className={css.avatar} />
+          <p className={css.name}>{username}</p>
+          <p className={css.tag}>{tag}</p>
+          <p className={css.location}>{location}</p>
+        </div>
 
-      <ul className={css.stats}>
-        <li className={css.item}>
-          <span className={css.label}>Followers</span>
-          <span className={css.quantity}>{followers}</span>
-        </li>
-        <li className={css.item}>
-          <span className={css.label}>Views</span>
-          <span className={css.quantity}>{views}</span>
-        </li>
-        <li className={css.item}>
-          <span className={css.label}>Likes</span>
-          <span className={css.quantity}>{likes}</span>
-        </li>
-      </ul>
+        <ul className={css.stats}>
+          <li className={css.item}>
+            <span className={css.label}>Followers</span>
+            <span className={css.quantity}>{followers}</span>
+          </li>
+          <li className={css.item}>
+            <span className={css.label}>Views</span>
+            <span className={css.quantity}>{views}</span>
+          </li>
+          <li className={css.item}>
+            <span className={css.label}>Likes</span>
+            <span className={css.quantity}>{likes}</span>
+          </li>
+        </ul>
+      </div>
     </div>
   );
 }
+
 Profile.propTypes = {
   username: PropTypes.string.isRequired,
   tag: PropTypes.string.isRequired,
